eadd063539701a1d2cef6468a4922373a2b55f01
Ken Chan <kchansf5@gmail.com>
2018-04-19 16:33:45 -0700

    Created #initialize of tiles

--- Minesweeper/board.rb
+++ Minesweeper/board.rb
@@ -27,4 +27,6 @@ class Board
   end
 
 
+
+
 end
---
+++ Minesweeper/tile.rb
@@ -0,0 +1,12 @@
+class Tile
+  attr_accessor :revealed, :bomb
+
+  def initialize(revealed = false, bomb= false, flagged = false)
+    @revealed = reveealed
+    @bomb = bomb
+    @flagged = flagged
+  end
+
+  
+
+end
